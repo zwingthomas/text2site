15f1276fded71e54136786ba56ef496e4a7236c8
zwingthomas <zwingthomas@gmail.com>
2024-10-20 17:17:01 -0500

    Allow for force delete of ecr for terraform delete

--- terraform-AWS/ecr.tf
+++ terraform-AWS/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "repo" {
   name                 = "${var.project_name}-repo"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   tags = {
     Name = "${var.project_name}-ecr-repo"
