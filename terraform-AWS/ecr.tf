7d1c345e11baad2f58aae80c26b505b8e0eeab23
zwingthomas <zwingthomas@gmail.com>
2024-10-24 18:17:19 -0500

    simplify

--- terraform-AWS/ecr.tf
+++ terraform-AWS/ecr.tf
@@ -7,8 +7,8 @@ data "aws_ecr_repository" "existing_repo" {
 # Create new repo if one does not exist
 resource "aws_ecr_repository" "repo" {
 
-  # Only build if the data block was unable to fetch an existing repo
-  count = length(data.aws_ecr_repository.existing_repo.id) == 0 ? 1 : 0
+  # Only build if one does not exist
+  count = var.create_ecr_repo ? 1 : 0
 
   name                 = "${var.project_name}-repo"
   image_tag_mutability = "MUTABLE"
