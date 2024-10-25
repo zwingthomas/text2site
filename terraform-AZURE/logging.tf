8a14a83f8d7261e3485822e7ffcb5edca87473b1
zwingthomas <zwingthomas@gmail.com>
2024-10-24 19:16:49 -0500

    declare logging categories one by one

--- terraform-AZURE/logging.tf
+++ terraform-AZURE/logging.tf
@@ -1,17 +1,41 @@
 # logging.tf
 
-resource "azurerm_monitor_diagnostic_setting" "aks_diagnostic" {
-  name               = "aks-diagnostic-setting"
-  target_resource_id = azurerm_kubernetes_cluster.aks_cluster.id
-
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+resource "azurerm_monitor_diagnostic_setting" "aks_diagnostic_kube_apiserver" {
+  name               = "aks-diagnostic-kube-apiserver"
+  target_resource_id = azurerm_kubernetes_cluster.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
 
   enabled_log {
-    category = ["kube-apiserver", "kube-controller-manager", "kube-scheduler", "kube-audit"]
+    category = "kube-apiserver"
   }
+}
 
-  metric {
-    category = "AllMetrics"
-    enabled  = true
+resource "azurerm_monitor_diagnostic_setting" "aks_diagnostic_kube_controller_manager" {
+  name               = "aks-diagnostic-kube-controller-manager"
+  target_resource_id = azurerm_kubernetes_cluster.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+
+  enabled_log {
+    category = "kube-controller-manager"
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "aks_diagnostic_kube_scheduler" {
+  name               = "aks-diagnostic-kube-scheduler"
+  target_resource_id = azurerm_kubernetes_cluster.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+
+  enabled_log {
+    category = "kube-scheduler"
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "aks_diagnostic_kube_audit" {
+  name               = "aks-diagnostic-kube-audit"
+  target_resource_id = azurerm_kubernetes_cluster.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+
+  enabled_log {
+    category = "kube-audit"
   }
 }
