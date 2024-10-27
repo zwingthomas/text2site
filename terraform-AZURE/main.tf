2940bea1f69c06cf64c473513b86a7b700dc1a6f
zwingthomas <zwingthomas@gmail.com>
2024-10-26 19:17:28 -0500

    No longer need the import workaround

--- terraform-AZURE/main.tf
+++ terraform-AZURE/main.tf
@@ -31,9 +31,24 @@ resource "azurerm_key_vault_secret" "twilio_auth_token" {
   depends_on   = [azurerm_role_assignment.aks_key_vault_access]
 }
 
-# Grant AKS access to Key Vault
-resource "azurerm_role_assignment" "aks_key_vault_access" {
+# Grant AKS Managed Identity Access to Key Vault
+resource "azurerm_role_assignment" "aks_mi_key_vault_access" {
+  scope                = azurerm_key_vault.key_vault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_kubernetes_cluster.aks_cluster.identity[0].principal_id
+}
+
+# Grant Kubelet Identity Access to Key Vault
+resource "azurerm_role_assignment" "aks_kubelet_key_vault_access" {
   scope                = azurerm_key_vault.key_vault.id
   role_definition_name = "Key Vault Reader"
   principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
+}
+
+# Store Twilio Auth Token in Key Vault
+resource "azurerm_key_vault_secret" "twilio_auth_token" {
+  name         = "twilio-auth-token"
+  value        = var.twilio_auth_token
+  key_vault_id = azurerm_key_vault.key_vault.id
+  depends_on   = [azurerm_role_assignment.aks_mi_key_vault_access, azurerm_role_assignment.aks_kubelet_key_vault_access]
 }
